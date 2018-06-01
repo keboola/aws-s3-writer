aacbd1d952e08cb74a6ff2dec1fd49fab48913b3
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-06-01 10:39:14 +0200

    fix: namespace

--- src/Component.php
+++ src/Component.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MyComponent;
+namespace Keboola\S3Writer;
 
 use Keboola\Component\BaseComponent;
 
--- src/run.php
+++ src/run.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Keboola\Component\UserException;
 use Keboola\Component\Logger;
-use MyComponent\Component;
+use Keboola\S3Writer\Component;
 
 require __DIR__ . '/../vendor/autoload.php';
 
